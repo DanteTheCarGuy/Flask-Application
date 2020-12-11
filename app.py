85bb911f89d36a99a75ce1a650ef7a7d785e16bc
DanteTheCarGuy <dantethecarguy@gmail.com>
2020-12-11 23:44:22 +0000

    Add option to Add category to db

--- app.py
+++ app.py
@@ -145,10 +145,25 @@ def delete_task(task_id):
     flash("Task Successfully Deleted")
     return redirect(url_for("get_tasks"))
 
+
 @app.route("/get_categories")
 def get_categories():
     categories = list(mongo.db.categories.find().sort("category_name", 1))
     return render_template("categories.html", categories=categories)
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.insert_one(category)
+        flash("New Category Added")
+        return redirect(url_for("get_categories"))
+
+    return render_template("add_categories.html")
+
 
 if __name__ == "_main_":
     app.run(host=os.environ.get("IP"),
